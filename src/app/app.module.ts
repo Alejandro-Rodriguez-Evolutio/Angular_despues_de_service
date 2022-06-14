6ce24f421d26434be941d4f508fb5379262aba20
Alejandro Rodriguez <alejandro.rodriguez@evolutio.com>
2022-06-14 16:47:38 +0200

    Introducir rutas a home,menu...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,64 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/* Nuevos modulos de modelo flexible */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
+
+import { MatListModule } from '@angular/material/list';
+
+
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+/*-------------------------------- */
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    /* Nuevos imports*/
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+
+    MatListModule,
+
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+
+    AppRoutingModule
+    /*------------------ */
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
